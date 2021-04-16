37726232fd996267e5e43740953b0d220382d4b2
Kosia2000 <kosia.babiuch@gmail.com>
2021-04-17 00:07:16 +0200

    Menu

---
+++ scenes/menu/StartMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _on_StartGameButton_pressed():
+	pass
+	
+func _on_ContinueGameButton_pressed():
+	pass
+	
+func _on_RestartGameButton_pressed():
+	pass	
+	
+func _on_BestScoreGameButton_pressed():
+	pass
+
+func _on_SettingsGameButton_pressed():
+	pass
+
+func _on_QuitGameButton_pressed():
+	get_tree().quit()
